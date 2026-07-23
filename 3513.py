8d98498def84e5c39ac90c9085e249dc3eda1df0
DavidsonRafaelK <davidson.rafael120@gmail.com>
2026-07-23 12:23:17 +0700

    solve 3513 using bit manipulation

---
+++ 3513.py
@@ -0,0 +1,15 @@
+nums = [1,2,3,4]
+
+if len(nums) == 1:
+    print(1)
+else:
+    print(2 ** len(nums).bit_length())
+
+# # n = 5
+# # print(n.bit_length())
+
+# n = 1
+# print(2 ** n.bit_length())
+
+# n = 2
+# print(2 ** n.bit_length())
